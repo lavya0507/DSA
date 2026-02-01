741e8b5b2ed41a60217f07711ba4e02af217cd25
lavya0507 <lavyagupta0507@gmail.com>
2026-02-01 23:23:21 +0530

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main() 
+{
+    int n, i, target, j, k;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int a1[n + 1]; 
+    for(k=0; k<n; k++) 
+    {  printf("Enter the element at position [%d]: ", k);
+       scanf("%d", &a1[k]); }
+    printf("\nEnter the index to delete: ");
+    scanf("%d", &j); 
+    for(k=j; k<n-1; k++)
+    { a1[k] = a1[k+1]; }
+    n--;
+    printf("\nEnter the number you want to add: ");
+    scanf("%d", &target);
+    printf("Enter the index to insert at: ");
+    scanf("%d", &i);
+    for(k=n; k>i; k--) 
+    {  a1[k] = a1[k-1]; }
+    a1[i] = target;
+    n++;
+    printf("\nFinal Array: ");
+    for(k=0; k<n; k++) 
+    { printf("%d ", a1[k]); }
+    return 0;
+}
