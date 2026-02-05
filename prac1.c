39b9f13ad5f7c4b027ec1069c9f0e236e6374312
lavya0507 <lavyagupta0507@gmail.com>
2026-02-05 23:40:53 +0530

    Add files via upload

---
+++ prac1.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int i, j, n, m;
+    printf("Enter the size of the first array: ");
+    scanf("%d", &n);
+    printf("Enter the size of the second array: ");
+    scanf("%d", &m);
+    int arr1[n+m], arr2[m];
+    printf("First array: \n");
+    for(i=0; i<n; i++)
+    { printf("Enter the element at position [%d]: ", i);
+      scanf("%d", &arr1[i]); }
+    printf("Second array: \n");
+    for(i=0; i<m; i++)
+    { printf("Enter the element at position [%d]: ", i);
+      scanf("%d", &arr2[i]); }
+    for(i=0; i<m; i++)
+    { arr1[i+m] = arr2[i]; }
+
+    for (i = 0; i < n + m - 1; i++)
+{
+    for (j = 0; j < n + m - i - 1; j++)
+    {
+        if (arr1[j] > arr1[j + 1])
+        {
+            int temp = arr1[j];
+            arr1[j] = arr1[j + 1];
+            arr1[j + 1] = temp;
+        }
+    }
+}
+
+    printf("\n");
+    for(i=0; i<n+m; i++)
+    { printf("%d", arr1[i]); }
+    return 0;
+}
