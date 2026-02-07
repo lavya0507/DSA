337673240a98ff4538c4bdc810ce7e3c7d28dcbc
lavya0507 <lavyagupta0507@gmail.com>
2026-02-07 21:31:34 +0530

    Add files via upload

--- Q6.c
+++ Q6.c
@@ -11,11 +11,11 @@ int main()
    for(i=1; i<n; i++)
    { if(arr[i] != arr[i-1])
      { arr[j] = arr[i];
-       j++;
-       n--; }
+       j++; }
    }
-   for(i=0; i<n; i++)
+   for(i=0; i<j; i++)
    { printf("%d", arr[i]); }
    return 0;
 }
+
 
