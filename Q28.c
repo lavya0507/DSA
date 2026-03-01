23bd0766e7908e085d72ed60d64b862b0c6a74b9
lavya0507 <lavyagupta0507@gmail.com>
2026-03-02 00:04:42 +0530

    Add files via upload

--- Q28.c
+++ Q28.c
@@ -1,39 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
-struct Node {
-    int data;
-    struct Node* next;
-};
-struct Node* createNode(int value) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = value;
-    newNode->next = NULL;
-    return newNode;
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+  int data;
+  struct node *next;
+} node;
+node *createnode(int val) {
+  node *new_node = malloc(sizeof(node));
+  new_node->data = val;
+  new_node->next = NULL;
+  return new_node;
 }
-int main() {
-    int n, value;
-    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
-    if (scanf("%d", &n) != 1 || n <= 0) return 0;
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &value);
-        newNode = createNode(value);
-        if (head == NULL) {
-            head = newNode;
-            newNode->next = head;
-            temp = head;
-        } else {
-            temp->next = newNode;
-            newNode->next = head;
-            temp = newNode;
-        }
-    }
-    if (head != NULL) {
-        struct Node* current = head;
-        do {
-            printf("%d ", current->data);
-            current = current->next;
-        } while (current != head);
-    }
-
-    return 0;
+node *createlinkedlist(int num) {
+  int i, val;
+  node *head = NULL;
+  node *tail = NULL;
+  for(i=0; i<num; i++)
+  { printf("Enter the value at node %d: ", i+1);
+    scanf("%d", &val);
+    node *temp = createnode(val);
+    if(head == NULL)
+    { head = temp;
+      tail = head; }
+    else
+    { tail->next = temp;
+      tail = temp; }
+  }
+  return head;
+}
+void Traverse(node *head) {
+  node *temp = head;
+  printf("\nLinked List: ");
+  while(temp != NULL)
+  { printf("%d ", temp->data);
+    temp = temp->next; }
+}
+node *Rotate(node *head, int k){
+  node *curr = head;
+  node *temp = head;
+  int i, count=0;
+  while(curr != NULL)
+  { count++;
+    curr = curr->next; }
+  curr = head;
+  k = k % count;
+  int step = count - k;
+  while(curr->next != NULL)
+  { curr = curr->next; }
+  curr->next = head;
+  for(i=1; i<step; i++)
+  { temp = temp->next; }
+  node *new_head = temp->next;
+  temp->next = NULL;
+  return new_head;
+}
+int main()
+{
+  int i, n, k;
+  printf("Enter number of nodes: ");
+  scanf("%d", &n);
+  node *link_list = createlinkedlist(n);
+  Traverse(link_list);
+  printf("\nEnter number of times you want to rotate the list: ");
+  scanf("%d", &k);
+  link_list = Rotate(link_list, k);
+  Traverse(link_list);
+  return 0;
 }
