d8493d25fa191bf38a52f2ab22b4d3afbb610eca
lavya0507 <lavyagupta0507@gmail.com>
2026-03-18 22:41:31 +0530

    Add files via upload

---
+++ Q46.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void printLevelOrder(struct Node* root) {
+    if (root == NULL) return;
+
+    struct Node* queue[1000];
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+
+    while (front < rear)
+    { struct Node* temp = queue[front++];
+      printf("%d ", temp->data);
+     if(temp->left != NULL)
+     { queue[rear++] = temp->left; }
+     if(temp->right != NULL)
+     { queue[rear++] = temp->right; }
+    }
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    printf("Level Order Traversal: ");
+    printLevelOrder(root);
+    
+    return 0;
+}
