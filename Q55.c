0cd3b852062861e2b56b1bdba5ae514669dd47e4
lavya0507 <lavyagupta0507@gmail.com>
2026-03-27 23:02:18 +0530

    Add files via upload

---
+++ Q55.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+void rightView(struct Node* root) {
+    if(root == NULL) return;
+    struct Node* queue[2000];
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+
+    while(front < rear)
+    {
+        int size = rear - front;
+        for(int i = 0; i < size; i++)
+        {
+            struct Node* curr = queue[front++];
+            if(i == size - 1)
+                printf("%d ", curr->data);
+            if(curr->left)
+                queue[rear++] = curr->left;
+            if(curr->right)
+                queue[rear++] = curr->right;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    struct Node* nodes[n];
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(nodes[i] != NULL)
+        {
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+            if(left < n)
+                nodes[i]->left = nodes[left];
+            if(right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    struct Node* root = nodes[0];
+    rightView(root);
+    return 0;
+}
