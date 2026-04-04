819929b45aed7d21ca841bf67cd5ce68f6c221a3
lavya0507 <lavyagupta0507@gmail.com>
2026-04-04 22:17:11 +0530

    Add files via upload

---
+++ Q62.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+  int data1;
+  int data2;
+  struct node *next;
+} node;
+node *createnode(int val1, int val2) {
+  node *new_node = malloc(sizeof(node));
+  new_node->data1 = val1;
+  new_node->data2 = val2;
+  new_node->next = NULL;
+  return new_node;
+}
+node *createlinkedlist(int num) {
+  int i, val1, val2;
+  node *head = NULL;
+  node *tail = NULL;
+  for(i=0; i<num; i++)
+  { printf("Enter the coefficient at node %d: ", i+1);
+    scanf("%d", &val1);
+    printf("Enter the power at node %d: ", i+1);
+    scanf("%d", &val2);
+    node *temp = createnode(val1, val2);
+    if(head == NULL)
+    { head = temp;
+      tail = head; }
+    else
+    { tail->next = temp;
+      tail = temp; }
+  }
+  return head;
+}
+void Traverse(node *head) {
+  node *temp = head;
+  printf("\nLinked List:\n");
+  while(temp != NULL)
+  { printf("%d ", temp->data1);
+    printf("%d\n", temp->data2);
+    temp = temp->next; }
+}
+node *reverse(node *head) { //Function to reverse a linked list
+  node *rev = NULL;
+  node *curr = head;
+  node *next = NULL;
+  while(curr != NULL)
+  { next = curr->next;
+    curr->next = rev;
+    rev = curr;
+    curr = next; }
+    return rev;
+}
+void polynomial(node *head) {
+  node *poly = head;
+  printf("The polynomial is: ");
+  while(poly != NULL)
+  { if(poly->data2 == 0)
+    { printf(" %d", poly->data1); }
+    else
+    { printf("%dx^%d +", poly->data1, poly->data2); }
+    poly = poly->next;
+  }
+}
+int main()
+{
+  int i, n, k, choice;
+  printf("Enter number of nodes: ");
+  scanf("%d", &n);
+  node *link_list = createlinkedlist(n);
+  polynomial(link_list);
+  if(choice == 4)
+  { node *rev = reverse(link_list);
+    Traverse(rev); }
+  return 0;
+}
