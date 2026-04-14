b67e1a754e92ebbab1b39622f7820f97b6dd2498
lavya0507 <lavyagupta0507@gmail.com>
2026-04-14 20:22:20 +0530

    Add files via upload

--- Q42.c
+++ Q42.c
@@ -1,25 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 int main() {
-    int n;
-    if (scanf("%d", &n) != 1) return 0;
-
-    // We can use an array as our "stack" to store N integers
+    int n, i;
+    if(scanf("%d", &n) != 1) return 0;
     int *stack = (int *)malloc(n * sizeof(int));
-
-    // 1. Read input and push onto the stack
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &stack[i]);
-    }
-
-    // 2. Pop from the stack (print from end to start)
-    for (int i = n - 1; i >= 0; i--) {
-        printf("%d", stack[i]);
-        if (i > 0) printf(" ");
+    for(i=0; i<n; i++)
+    { scanf("%d", &stack[i]); }
+    for(i=n-1; i>=0; i--)
+    { printf("%d", stack[i]);
+      if(i > 0) printf(" ");
     }
     printf("\n");
-
     free(stack);
     return 0;
 }
