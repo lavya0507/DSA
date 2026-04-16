1a686b5bb380b0839824eb9019d8b1b8114f01ce
lavya0507 <lavyagupta0507@gmail.com>
2026-04-16 21:00:12 +0530

    Add files via upload

---
+++ Q74.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int compare(const void *a, const void *b) {
+    return strcmp(*(const char **)a, *(const char **)b);
+}
+int main() {
+    int n, i;
+    if(scanf("%d", &n) != 1) return 0;
+    char **votes = (char **)malloc(n * sizeof(char *));
+    for(i=0; i<n; i++)
+    { votes[i] = (char *)malloc(100 * sizeof(char));
+      scanf("%s", votes[i]); }
+    qsort(votes, n, sizeof(char *), compare);
+    char *winner = votes[0];
+    int max_votes = 0;
+    int current_votes = 1;
+    for(i=1; i<=n; i++)
+    { if(i < n && strcmp(votes[i], votes[i - 1]) == 0)
+      { current_votes++; }
+      else
+      { if(current_votes > max_votes)
+        { max_votes = current_votes;
+          winner = votes[i - 1];
+        }
+        current_votes = 1;
+      }
+    }
+    printf("%s %d\n", winner, max_votes);
+    for(i=0; i<n; i++) free(votes[i]);
+    free(votes);
+    return 0;
+}
