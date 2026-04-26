4381df0f843438730a9bebf0899bb568bedf51bc
lavya0507 <lavyagupta0507@gmail.com>
2026-04-26 23:04:39 +0530

    Add files via upload

---
+++ Q84.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        { arr[j + 1] = arr[j];
+          j = j - 1; }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n;
+    if(scanf("%d", &n) != 1) return 0;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    { scanf("%d", &arr[i]); }
+    insertionSort(arr, n);
+    for (int i = 0; i < n; i++)
+    { printf("%d%s", arr[i], (i == n - 1) ? "" : " "); }
+    printf("\n");
+    return 0;
+}
