92d07ac9278471bd1f84ddb88687b82e7bca86fe
lavya0507 <lavyagupta0507@gmail.com>
2026-04-27 23:08:32 +0530

    Add files via upload

--- Q84.c
+++ Q84.c
@@ -1,25 +1,43 @@
 #include<stdio.h>
+#include<stdlib.h>
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int L[n1], R[n2];
+    for(int i=0; i<n1; i++)
+    { L[i] = arr[left + i]; }
+    for(int j=0; j<n2; j++)
+    { R[j] = arr[mid + 1 + j]; }
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2)
+    { if (L[i] <= R[j])
+      { arr[k] = L[i++]; }
+      else
+      { arr[k] = R[j++]; }
+      k++; }
+    while(i < n1) arr[k++] = L[i++];
+    while(j < n2) arr[k++] = R[j++];
+}
 
-void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-        while (j >= 0 && arr[j] > key)
-        { arr[j + 1] = arr[j];
-          j = j - 1; }
-        arr[j + 1] = key;
+void mergeSort(int arr[], int left, int right) {
+    if(left < right)
+    { int mid = left + (right - left) / 2;
+      mergeSort(arr, left, mid);
+      mergeSort(arr, mid + 1, right);
+      merge(arr, left, mid, right);
     }
 }
 
-int main() {
-    int n;
+int main()
+{
+    int i, n;
     if(scanf("%d", &n) != 1) return 0;
-    int arr[n];
-    for(int i = 0; i < n; i++)
+    int *arr = (int *)malloc(n * sizeof(int));
+    for(i=0; i<n; i++)
     { scanf("%d", &arr[i]); }
-    insertionSort(arr, n);
-    for (int i = 0; i < n; i++)
-    { printf("%d%s", arr[i], (i == n - 1) ? "" : " "); }
-    printf("\n");
+    mergeSort(arr, 0, n - 1);
+    for(i=0; i<n; i++)
+    { printf("%d%c", arr[i], (i == n - 1) ? '\n' : ' '); }
+    free(arr);
     return 0;
 }
